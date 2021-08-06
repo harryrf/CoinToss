f37954c7ee4ceb4a6fe9a6aa2122e6064f942f4e
Harry Fahringer III <harryrf@users.noreply.github.com>
2021-08-05 22:25:38 -0400

    Created a cointoss game (unfinished)

---
+++ CoinToss/src/CoinToss.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class CoinToss {
+
+public String tossACoin() {
+  Random rand = new Random();
+  int toss = Math.abs(rand.nextInt()) % 2;
+
+  if (toss == 0) {
+      return "Heads";
+  } else {
+      return "Tails";
+  }
+}
+
+
+  public static void main(String[] args) throws Exception {
+      CoinToss game = new CoinToss();
+
+      System.out.println(game.tossACoin());
+  }
+}
